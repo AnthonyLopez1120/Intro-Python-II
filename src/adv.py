7335c94499ae50f414abd99222c58fd585385a38
Anthony Lopez <anthonybeconscious@gmail.com>
2020-04-10 08:54:50 -0500

    mvpish

--- src/adv.py
+++ src/adv.py
@@ -1,17 +1,19 @@
 from room import Room
+from player import Player
+from items import torch, sword
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     f"North of you, the cave mount beckons. A {torch.description} is here", torch),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", f"""A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm. You see a {sword.description}"""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
@@ -39,6 +41,10 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(input("What's your name? "), room["outside"])
+
+print(f"Hello, {player.name} the brave!")
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +55,28 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print(player.current_room)
+while True:
+    direction = input("Enter a direction ").lower()
+    if direction in ["n", "s", "e", "w"]:
+        player.move(direction)
+    elif direction == "p":
+        player.pickup_item(room.items)
+        print("You picked up an item")
+        print(player.inventory)
+    elif direction == "d":
+        player.drop_item(player.inventory)
+        print("you dropped an item")
+    elif direction == "i":
+            if len(player.inventory) < 1:
+                print("you have no items")
+            else:
+                print(f"You are holding {player.inventory}")
+        
+        
+    elif direction == "q":
+        print("Adios Amigo!")
+        exit()
+else:
+    print("Please enter a direction, n, s, e, or w")
